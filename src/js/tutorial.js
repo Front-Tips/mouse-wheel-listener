2d43fbc6733b4e182266652bee53d1d7e25aab6e
Arbaoui Mehdi <arbaoui.mehdi@gmail.com>
2024-03-10 10:43:29 +0000

    Update code structure

--- src/js/tutorial.js
+++ src/js/tutorial.js
@@ -1,28 +1,15 @@
-/* ------------------------ */
-/*      scrollDirection     */
-/* ------------------------ */
 function scrollDirection(e) {
   // Define the scroll direction
-  // based on deltaY value
-  const direction =
-    e.deltaY > 0 ? "down" : "up";
+  // Based on `deltaY` value
+  const direction = e.deltaY > 0 ? "down" : "up";
 
-  // Choose the appropriate emoji
-  // for the direction
-  const emoji =
-    direction === "down" ? "👇" : "👆";
-
-  // Update `innetHTML`
-  document.getElementById(
-    "message"
-  ).innerHTML = `<span>${emoji}</span> ${direction}`;
+  document.getElementById("message").innerHTML =
+    direction === "up"
+      ? // When scrolling up
+        `<span>👆</span> scroll ${direction}`
+      : // When scrolling down
+        `scroll ${direction} <span>👇</span>`;
 }
 
-/* ------------------------ */
-/*          Events          */
-/* ------------------------ */
 // Add the `wheel` event listener
-window.addEventListener(
-  "wheel",
-  scrollDirection
-);
+window.addEventListener("wheel", scrollDirection);
